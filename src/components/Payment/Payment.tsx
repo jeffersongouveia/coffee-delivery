2912cae857ab7b85445bf70042646f409dd55f13
Jeff Gouveia <jeff.gouveia@hotmail.com>
2024-02-02 09:50:45 -0400

    Finished Payment card

--- src/components/Payment/Payment.tsx
+++ src/components/Payment/Payment.tsx
@@ -1,9 +1,45 @@
-import { Container, Title } from './styles.ts'
+import { Bank, CreditCard, CurrencyDollar, Money } from '@phosphor-icons/react'
+
+import {
+  Container,
+  OptionCard,
+  Options,
+  PaymentContainer,
+  PaymentHeader,
+  PaymentHeaderContainer,
+} from './styles.ts'
+import { defaultTheme } from '../../styles/themes.ts'
 
 export default function Payment() {
   return (
     <Container>
-      <Title>Payment</Title>
+      <PaymentContainer>
+        <PaymentHeaderContainer>
+          <CurrencyDollar size={22} color={defaultTheme.colors.brand.purple} />
+
+          <PaymentHeader>
+            <h4>Payment</h4>
+            <p>Payment is made upon delivery. Choose the way you want to pay</p>
+          </PaymentHeader>
+        </PaymentHeaderContainer>
+
+        <Options>
+          <OptionCard selected>
+            <CreditCard size={16} color={defaultTheme.colors.brand.purple} />
+            <p>Credit Card</p>
+          </OptionCard>
+
+          <OptionCard>
+            <Bank size={16} color={defaultTheme.colors.brand.purple} />
+            <p>Debit Card</p>
+          </OptionCard>
+
+          <OptionCard>
+            <Money size={16} color={defaultTheme.colors.brand.purple} />
+            <p>Money</p>
+          </OptionCard>
+        </Options>
+      </PaymentContainer>
     </Container>
   )
 }
--- src/components/Payment/styles.ts
+++ src/components/Payment/styles.ts
@@ -1,14 +1,12 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+interface OptionCardProps {
+  selected?: boolean
+}
 
 export const Container = styled.div`
   display: grid;
   width: 100%;
-`
-
-export const Title = styled.h2`
-  font: ${(props) => props.theme.fonts.text.m};
-  color: ${(props) => props.theme.colors.base.subtitle};
-  margin-bottom: 15px;
 `
 
 export const PaymentContainer = styled.div`
@@ -17,4 +15,60 @@ export const PaymentContainer = styled.div`
   background: ${(props) => props.theme.colors.base.card};
   border-radius: 6px;
   padding: 40px;
+`
+
+export const PaymentHeaderContainer = styled.div`
+  display: flex;
+  gap: 8px;
+`
+
+export const PaymentHeader = styled.div`
+  display: grid;
+  gap: 2px;
+
+  h4 {
+    font: ${(props) => props.theme.fonts.text.m};
+    color: ${(props) => props.theme.colors.base.subtitle};
+  }
+
+  p {
+    font: ${(props) => props.theme.fonts.text.s};
+    color: ${(props) => props.theme.colors.base.text};
+  }
+`
+
+export const Options = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+`
+
+export const OptionCard = styled.button<OptionCardProps>`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 12px;
+  padding: 12px;
+  border-radius: 6px;
+  cursor: pointer;
+  border: none;
+  box-shadow: none;
+  min-width: 180px;
+  background: ${(props) => props.theme.colors.base.button};
+
+  ${(props) =>
+    props.selected &&
+    css`
+      border: 1px solid ${props.theme.colors.brand.purple};
+    `}
+
+  &:hover {
+    background: ${(props) => props.theme.colors.base.hover};
+  }
+
+  p {
+    font: ${(props) => props.theme.fonts.button.m};
+    color: ${(props) => props.theme.colors.base.text};
+    text-transform: uppercase;
+  }
 `
