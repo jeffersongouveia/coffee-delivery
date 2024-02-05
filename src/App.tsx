332a9fdfd04ecea15712b20ede7262027a4c621d
Jeff Gouveia <jeff.gouveia@hotmail.com>
2024-02-05 08:59:19 -0400

    Created coffee context and added on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { ThemeProvider } from 'styled-components'
 import { BrowserRouter } from 'react-router-dom'
 
+import Router from './Router.tsx'
+import CoffeesContextProvider from './contexts/CoffeesContext.tsx'
+
 import GlobalStyles from './styles/global.ts'
 import { defaultTheme } from './styles/themes.ts'
-import Router from './Router.tsx'
 
 export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CoffeesContextProvider>
+          <Router />
+        </CoffeesContextProvider>
         <GlobalStyles />
       </BrowserRouter>
     </ThemeProvider>
---
+++ src/contexts/CoffeesContext.tsx
@@ -0,0 +1,198 @@
+import React, { createContext, useState } from 'react'
+
+import traditionalEspresso from '../assets/coffees/traditional-expresso.png'
+import americanEspresso from '../assets/coffees/american-expresso.png'
+import creamyEspresso from '../assets/coffees/creamy-espresso.png'
+import icedEspresso from '../assets/coffees/iced-espresso.png'
+import coffeeWithMilk from '../assets/coffees/coffee-with-milk.png'
+import latte from '../assets/coffees/latte.png'
+import cappuccino from '../assets/coffees/cappuccino.png'
+import macchiato from '../assets/coffees/macchiato.png'
+import mochaccino from '../assets/coffees/mochaccino.png'
+import hotChocolate from '../assets/coffees/hot-chocolate.png'
+import cuban from '../assets/coffees/cuban.png'
+import hawaiian from '../assets/coffees/hawaiian.png'
+import arabic from '../assets/coffees/arabic.png'
+import irish from '../assets/coffees/irish.png'
+
+export interface CoffeeType {
+  name: string
+  description: string
+  image: string
+  price: number
+  tags: string[]
+  quantity: number
+}
+
+interface CoffeesContextType {
+  allCoffees: CoffeeType[]
+  coffeesInCart: CoffeeType[]
+  addCoffeeToCart: (coffee: CoffeeType) => void
+  updateCoffeeQuantity: (coffeeName: string, quantity: number) => void
+}
+
+interface CoffeesProviderProps {
+  children: React.ReactNode
+}
+
+export const CoffeesContext = createContext<CoffeesContextType>(
+  {} as CoffeesContextType,
+)
+
+enum TAGS {
+  TRADITIONAL = 'TRADITIONAL',
+  ALCOHOLIC = 'ALCOHOLIC',
+  COLD = 'COLD',
+  WITH_MILK = 'WITH MILK',
+  ESPECIAL = 'ESPECIAL',
+}
+
+const allCoffees: CoffeeType[] = [
+  {
+    image: traditionalEspresso,
+    name: 'Traditional Espresso',
+    description: 'Traditional coffee made with hot water and ground beans',
+    tags: [TAGS.TRADITIONAL],
+    price: 9.9,
+    quantity: 1,
+  },
+  {
+    image: americanEspresso,
+    name: 'American Espresso',
+    description: 'Diluted espresso, less intense than traditional',
+    tags: [TAGS.TRADITIONAL],
+    price: 8.9,
+    quantity: 1,
+  },
+  {
+    image: creamyEspresso,
+    name: 'Creamy Espresso',
+    description: 'Traditional espresso with creamy foam',
+    tags: [TAGS.TRADITIONAL],
+    price: 10.9,
+    quantity: 1,
+  },
+  {
+    image: icedEspresso,
+    name: 'Iced Espresso',
+    description: 'Drink prepared with espresso and ice cubes',
+    tags: [TAGS.TRADITIONAL, TAGS.COLD],
+    price: 9.9,
+    quantity: 1,
+  },
+  {
+    image: coffeeWithMilk,
+    name: 'Coffee with Milk',
+    description: 'Half and half traditional espresso with steamed milk',
+    tags: [TAGS.TRADITIONAL, TAGS.WITH_MILK],
+    price: 11.9,
+    quantity: 1,
+  },
+  {
+    image: latte,
+    name: 'Latte',
+    description: 'A shot of espresso with double the milk and creamy foam',
+    tags: [TAGS.TRADITIONAL, TAGS.WITH_MILK],
+    price: 12.6,
+    quantity: 1,
+  },
+  {
+    image: cappuccino,
+    name: 'Cappuccino',
+    description:
+      'Cinnamon drink made from equal doses of coffee, milk and foam',
+    tags: [TAGS.TRADITIONAL, TAGS.WITH_MILK],
+    price: 11.9,
+    quantity: 1,
+  },
+  {
+    image: macchiato,
+    name: 'Macchiato',
+    description: 'Espresso mixed with some hot milk and froth',
+    tags: [TAGS.TRADITIONAL, TAGS.WITH_MILK],
+    price: 13.9,
+    quantity: 1,
+  },
+  {
+    image: mochaccino,
+    name: 'Mochaccino',
+    description: 'Espresso with chocolate syrup, little milk and foam',
+    tags: [TAGS.TRADITIONAL, TAGS.WITH_MILK],
+    price: 15.8,
+    quantity: 1,
+  },
+  {
+    image: hotChocolate,
+    name: 'Hot Chocolate',
+    description: 'Drink made with chocolate dissolved in hot milk and coffee',
+    tags: [TAGS.ESPECIAL, TAGS.WITH_MILK],
+    price: 12.9,
+    quantity: 1,
+  },
+  {
+    image: cuban,
+    name: 'Cuban',
+    description: 'Iced espresso drink with rum, cream and mint',
+    tags: [TAGS.ESPECIAL, TAGS.ALCOHOLIC, TAGS.COLD],
+    price: 18.9,
+    quantity: 1,
+  },
+  {
+    image: hawaiian,
+    name: 'Hawaiian',
+    description: 'Sweet drink prepared with coffee and coconut milk',
+    tags: [TAGS.ESPECIAL],
+    price: 14.8,
+    quantity: 1,
+  },
+  {
+    image: arabic,
+    name: 'Arabic',
+    description: 'Drink prepared with Arabic coffee beans and spices',
+    tags: [TAGS.ESPECIAL],
+    price: 13.9,
+    quantity: 1,
+  },
+  {
+    image: irish,
+    name: 'Irish',
+    description:
+      'Drink made from coffee, Irish whiskey, sugar and whipped cream',
+    tags: [TAGS.ESPECIAL, TAGS.ALCOHOLIC],
+    price: 16.9,
+    quantity: 1,
+  },
+]
+
+export default function CoffeesContextProvider(props: CoffeesProviderProps) {
+  const [coffees, setCoffees] = useState<CoffeeType[]>([])
+
+  function addCoffeeToCart(coffee: CoffeeType) {
+    setCoffees([...coffees, coffee])
+  }
+
+  function updateCoffeeQuantity(coffeeName: string, quantity: number) {
+    const updatedCoffees = coffees.map((coffee) => {
+      if (coffee.name === coffeeName) {
+        coffee.quantity = quantity
+      }
+
+      return coffee
+    })
+
+    setCoffees(updatedCoffees)
+  }
+
+  return (
+    <CoffeesContext.Provider
+      value={{
+        allCoffees,
+        coffeesInCart: coffees,
+        addCoffeeToCart,
+        updateCoffeeQuantity,
+      }}
+    >
+      {props.children}
+    </CoffeesContext.Provider>
+  )
+}
